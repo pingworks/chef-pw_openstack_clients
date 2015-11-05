c494cfff1339f3a64d6c39aa32fbd6d62cb5d163
Christoph Lukas <lukas@pingworks.de>
2015-11-05 14:50:15 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'Apache 2.0'
 description      'Installs/Configures pw_openstack_clients'
 long_description 'Installs/Configures pw_openstack_clients'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'apt'
